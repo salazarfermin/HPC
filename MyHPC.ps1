bc1f230b64447bfbf6c121bc683da0d35c960b1a
salazarfermin <salazarfermin@hotmail.com>
2016-12-27 17:41:59 -0600

    Add files via upload

---
+++ MyHPC.ps1
@@ -0,0 +1,14 @@
+﻿[string]$SubscriptionId = 'a3031bbc-4497-4414-90df-e927b85922f9';
+#[string]$SubscriptionId = '86e16eab-9149-438c-b23f-042a43311e1c';
+[string]$ResourceGroupName = 'DCPNTI-GSPI-HPC';
+[string]$Location = 'South Central US';
+
+Login-AzureRmAccount -SubscriptionId $SubscriptionId;
+
+New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location;
+
+New-AzureRmResourceGroupDeployment -Name hpc-ctest-vnet -ResourceGroupName $ResourceGroupName -TemplateFile (Join-Path (Split-Path $myinvocation.mycommand.path) 'vnet\vnetdeploy.json') -TemplateParameterFile (Join-Path (Split-Path $myinvocation.mycommand.path) 'vnet\parameters.json') -Verbose;
+
+New-AzureRmResourceGroupDeployment -Name hpc-ctest -ResourceGroupName $ResourceGroupName -TemplateFile (Join-Path (Split-Path $myinvocation.mycommand.path) 'HPCLinux.json') -Verbose;
+
+Remove-AzureRmResourceGroup -Name $ResourceGroupName;
